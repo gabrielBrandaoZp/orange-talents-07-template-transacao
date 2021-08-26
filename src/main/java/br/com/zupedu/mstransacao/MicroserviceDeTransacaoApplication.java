e724f440a6b760acf8a4036c68ec5daf2c2fd877
Gabriel Brandão <gabriel.brandao@zup.com.br>
2021-08-26 11:35:31 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupedu/mstransacao/MicroserviceDeTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.mstransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDeTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDeTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupedu/mstransacao/MicroserviceDeTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.mstransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDeTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

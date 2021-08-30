1cbe4253f608409497cd9ebe01dfa05dd92ac64a
Gabriel Brandão <gabriel.brandao@zup.com.br>
2021-08-30 10:37:03 -0300

    add funcionalidade de autorização e autenticação

---
+++ src/main/java/br/com/zupedu/mstransacao/security/ResourceServerConfig.java
@@ -0,0 +1,20 @@
+package br.com.zupedu.mstransacao.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers(HttpMethod.GET, "/api/v1/compras/**").hasAuthority("SCOPE_escopo-transacoes")
+                .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
